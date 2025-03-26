559910f2dbb8467bea1103643da4b2d8c72a6b20
May-b-abbe <abbelam7@gmail.com>
2025-03-26 16:28:50 +1300

    Create .py

---
+++ .py
@@ -0,0 +1,16 @@
+for i in range(2):
+    print("hello world")
+    print("5*2")
+    print(5*2)
+#comment
+"""
+comment
+"""
+print(10+2)
+print(10-2)
+print(10/2)
+print(10*2)
+statement="hello world"
+print(statement)
+x=int(input("enter your value in minutes:"))
+print(x*60)
