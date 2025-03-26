732a12ed87437fcf765e332eb3437d67319e3930
May-b-abbe <abbelam7@gmail.com>
2025-03-26 17:51:56 +1300

    Create problem 1.py

---
+++ problem 1.py
@@ -0,0 +1,11 @@
+#question 1
+x=int(input("enter your value in minutes"))
+print(x*60)
+#question 2
+y=int(input("amount of cards Cruella recieved"))
+print("Jane recives at least",y*2,"cards")
+#question 3
+z=int(input("enter your age in years:"))
+a=(100*365)
+b=(a-z*365)
+print("You are",z*365,"days old.","In",b,"days you will be 100 years old.")
